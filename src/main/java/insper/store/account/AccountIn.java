ea858129c5cabd8bd684808e4fb5871d6ea04615
Humberto Sandmann <hsandmann@ieee.org>
2024-02-10 19:24:47 -0300

    new version

---
+++ src/main/java/insper/store/account/AccountIn.java
@@ -0,0 +1,14 @@
+package insper.store.account;
+
+import lombok.Builder;
+import lombok.experimental.Accessors;
+
+@Builder
+@Accessors(fluent = true, chain = true)
+public record AccountIn (
+    String name,
+    String email,
+    String password
+) {
+    
+}
---
+++ src/main/java/insper/store/account/AccountOut.java
@@ -0,0 +1,14 @@
+package insper.store.account;
+
+import lombok.Builder;
+import lombok.experimental.Accessors;
+
+@Builder
+@Accessors(fluent = true, chain = true)
+public record AccountOut (
+    String id,
+    String name,
+    String email
+) {
+    
+}
