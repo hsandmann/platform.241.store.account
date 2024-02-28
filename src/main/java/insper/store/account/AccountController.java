859e331c1d5b71336bbfe7cb3fa51610dc1e76ff
Humberto Sandmann <hsandmann@ieee.org>
2024-02-28 11:52:55 -0300

    new version

--- src/main/java/insper/store/account/AccountController.java
+++ src/main/java/insper/store/account/AccountController.java
@@ -1,11 +1,13 @@
 package insper.store.account;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@FeignClient(name = "store-account")
 public interface AccountController {
 
     @PostMapping("/accounts")
