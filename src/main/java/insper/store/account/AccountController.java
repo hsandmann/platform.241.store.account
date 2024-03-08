b944b4c8537b17f6090c4ee7517567f2b519ba7a
Humberto Sandmann <hsandmann@ieee.org>
2024-03-08 12:42:02 -0300

    new version

--- src/main/java/insper/store/account/AccountController.java
+++ src/main/java/insper/store/account/AccountController.java
@@ -2,10 +2,12 @@ package insper.store.account;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "store-account")
 public interface AccountController {
@@ -24,6 +26,12 @@ public interface AccountController {
     public ResponseEntity<AccountOut> update(
         @PathVariable(required = true) String id,
         @RequestBody(required = true) AccountIn in
+    );
+
+    @GetMapping("/accounts")
+    public ResponseEntity<AccountOut> read(
+        @RequestHeader(required = true, name = "id-user") String idUser,
+        @RequestHeader(required = true, name = "role-user") String roleUser
     );
     
 }
