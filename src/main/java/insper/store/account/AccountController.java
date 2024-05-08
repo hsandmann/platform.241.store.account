e2bca407bcb4a82930677ece194ae702d8020880
Humberto Sandmann <hsandmann@ieee.org>
2024-05-08 05:37:40 -0300

    k8s

--- src/main/java/insper/store/account/AccountController.java
+++ src/main/java/insper/store/account/AccountController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "store-account")
+@FeignClient(name = "account")
 public interface AccountController {
 
     @PostMapping("/accounts")
